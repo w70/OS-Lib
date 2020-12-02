dc1421f74640e18c2007e3cc7367d10904c3c05a
CHARLENE <3541765320@qq.com>
2020-12-03 01:14:23 +0800

    linux mytime

---
+++ lib 2/Program/hello2.cpp
@@ -0,0 +1,17 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"unistd.h"
+int main(int argc,char*argv[]){
+   
+    if(argc==1){
+        printf("hello\n");
+        return 0;
+    }
+    if(argc==2){
+        int time=atoi(argv[1]);
+        sleep(time);
+         printf("hello\n");
+         return 0;
+    }
+
+}
--- lib 2/Program/mytime2.cpp
+++ lib 2/Program/mytime2.cpp
@@ -1,79 +1,101 @@
-#include"sys/types.h"
-#include<unistd.h>
-#include"stdio.h"
-#include<sys/time.h>  //gettimeofday
-#include<sys/wait.h>  //wait
-#include<ctime>       //localtime
-int main(int argc,char*argv[]){
+#include "sys/types.h"
+#include <unistd.h>
+#include "stdio.h"
+#include <sys/time.h> //gettimeofday
+#include <sys/wait.h> //wait
+#include <ctime>      //localtime
+#include"string.h"
+#include"stdlib.h"
+int main(int argc, char *argv[])
+{
     pid_t pid;
-   
-     int year,month,day,hour,minute,second,millisecond;
-   struct timeval startTime;
+
+    int year, month, day, hour, minute, second, millisecond;
+    struct timeval startTime;
     struct timeval endTime;
     struct timezone zone;
     time_t startTime_t;
     time_t endTime_t;
     struct tm *startTime_tm;
     struct tm *endTime_tm;
-   
-    if((pid=fork())<0){
+
+    if ((pid = fork()) < 0)
+    {
         printf("fork failed\n");
         return 0;
-    }else if(pid==0){
-        if(argc==1)
-        execv(argv[1],argv);
-        else{
-            char *str = strcat(argv[1], " ");
-          str = strcat(str, argv[2]);
-           execv(str,argv);
+    }
+    else if (pid == 0)
+    {
+        
+        if (argc == 2)
+            execv(argv[1], argv);
+        else
+        {
+           argv++;
+            if(execv(argv[0], argv)==-1){
+                printf("error\n");
+            }
         }
-
-    }else{
-        gettimeofday(&startTime,&zone);
-        wait(0);   //µÈ´ý×Ó½ø³Ì½áÊø
+    }
+    else
+    {
+        gettimeofday(&startTime, &zone);
+        wait(0); 
         gettimeofday(&endTime, &zone);
-        long long int during=(endTime.tv_sec-startTime.tv_sec)*1000000+endTime.tv_usec-startTime.tv_usec;
-        millisecond=during%1000000;
-        second=during/1000000;
-        minute=second/60;
-        second%=60;
-        hour=minute/60;
-        minute%=60;
-        day=hour/24;
-        hour%=24;
-        month=day/30;
-        day%=30;
-        year=month/12;
-        month%=12;
-        if(second<0){
-        minute--;
-        second+=60;
+        long long int during = (endTime.tv_sec - startTime.tv_sec) * 1000000 + endTime.tv_usec - startTime.tv_usec;
+        millisecond = during % 1000000;
+        second = during / 1000000;
+        minute = second / 60;
+        second %= 60;
+        hour = minute / 60;
+        minute %= 60;
+        day = hour / 24;
+        hour %= 24;
+        month = day / 30;
+        day %= 30;
+        year = month / 12;
+        month %= 12;
+        if (second < 0)
+        {
+            minute--;
+            second += 60;
         }
-    if(minute<0){
-        hour--;
-        minute+=60;
-    }
-    if(hour<0){
-        day--;
-        hour+=24;
-    }
-    if(day<0){
-        month--;
-        day+=30;
-    }
-    if(month<0){
-        year--;
-        month+=12;
-    }
+        if (minute < 0)
+        {
+            hour--;
+            minute += 60;
+        }
+        if (hour < 0)
+        {
+            day--;
+            hour += 24;
+        }
+        if (day < 0)
+        {
+            month--;
+            day += 30;
+        }
+        if (month < 0)
+        {
+            year--;
+            month += 12;
+        }
 
-    if(year>0) printf("%d Äê ",year);
-    if(month>0) printf("%d ÔÂ ",month);
-   if(day>0) printf("%d ÈÕ ",day);
-   if(hour>0) printf("%d Ð¡Ê± ",hour);
-   if(minute>0) printf("%d ·ÖÖÓ ",minute);
-   if(second>0) printf("%d Ãë ",second);
-   printf("\n");
-    return 0;
+        if (year > 0)
+            printf("%d å¹´ ", year);
+        if (month > 0)
+            printf("%d æœˆ ", month);
+        if (day > 0)
+            printf("%d æ—¥ ", day);
+        if (hour > 0)
+            printf("%d æ—¶ ", hour);
+        if (minute > 0)
+            printf("%d åˆ† ", minute);
+        if (second > 0)
+            printf("%d ç§’ ", second);
+        if(millisecond>0)
+            printf("%d å¾®ç§’ ",millisecond);
+        printf("\n");
+        return 0;
     }
-
 }
